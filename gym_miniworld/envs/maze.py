85ace9fc6579371a6dd92d4311a2714e3df8426d
daddabarba <davidebarbieri97@gmail.com>
2020-09-04 19:31:47 +0200

    Added support to load and store maze map

--- gym_miniworld/envs/maze.py
+++ gym_miniworld/envs/maze.py
@@ -5,6 +5,8 @@ from ..miniworld import MiniWorldEnv, Room
 from ..entity import Box, ImageFrame
 from ..params import DEFAULT_PARAMS
 
+import json
+
 class Maze(MiniWorldEnv):
     """
     Maze environment in which the agent has to reach a red box
@@ -16,12 +18,16 @@ class Maze(MiniWorldEnv):
         num_cols=8,
         room_size=3,
         max_episode_steps=None,
+        load_from=None,
+        save_to=None,
         **kwargs
     ):
         self.num_rows = num_rows
         self.num_cols = num_cols
         self.room_size = room_size
         self.gap_size = 0.25
+        self.load_from = load_from
+        self.save_to = save_to
 
         super().__init__(
             max_episode_steps = max_episode_steps or num_rows * num_cols * 24,
@@ -61,6 +67,21 @@ class Maze(MiniWorldEnv):
 
         visited = set()
 
+        neighborhood = {}
+
+        if self.load_from is not None:
+            with open(self.load_from, 'rt') as f:
+                neighborhood = dict(map(
+                    lambda x : (
+                        tuple(map(
+                            lambda x : int(x),
+                            x[0][1:-1].split(','),
+                        )),
+                        x[1],
+                    ),
+                    json.load(f).items(),
+                ))
+
         def visit(i, j):
             """
             Recursive backtracking maze construction algorithm
@@ -72,7 +93,12 @@ class Maze(MiniWorldEnv):
             visited.add(room)
 
             # Reorder the neighbors to visit in a random order
-            neighbors = self.rand.subset([(0,1), (0,-1), (-1,0), (1,0)], 4)
+
+            if (j,i) not in neighborhood:
+                neighbors = self.rand.subset([(0,1), (0,-1), (-1,0), (1,0)], 4)
+                neighborhood[(j,i)] = neighbors
+            else:
+                neighbors = neighborhood[(j,i)]
 
             # For each possible neighbor
             for dj, di in neighbors:
@@ -102,6 +128,20 @@ class Maze(MiniWorldEnv):
         self.box = self.place_entity(Box(color='red'))
 
         self.place_agent()
+
+        if self.save_to is not None:
+            print(f"Saving map to {self.save_to}")
+            with open(self.save_to, 'wt') as f:
+                json.dump(
+                    dict(map(
+                        lambda x : (
+                            str(x[0]),
+                            x[1],
+                        ),
+                        neighborhood.items()
+                    )),
+                    f,
+                )
 
     def step(self, action):
         obs, reward, done, info = super().step(action)
